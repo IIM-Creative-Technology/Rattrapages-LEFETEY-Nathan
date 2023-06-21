3c6fdd16cc5c777f80f03efa00c233988fcd0507
Nathan Lefetey <lefetey.nathan@gmail.com>
2023-06-21 14:46:08 +0200

    :lipstick: Add some style to day card

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,11 @@ let API_CITY = "Paris"
 let DAYS_RANGE = 14
 
 const daysSection = document.getElementById('days-data-section')
+const showFirst = document.getElementById('show-first')
+showFirst.addEventListener('click', () => {
+    document.getElementById('day-2023-06-21').classList.remove('hidden')
+    document.getElementById('day-2023-06-21').classList.add('flex')
+})
 
 window.onload = getWeatherData();
 async function getWeatherData() {
@@ -32,6 +37,7 @@ async function getWeatherData() {
 
                 let dayCard = document.createElement('div')
                 dayCard.id = `day-${date}`
+                dayCard.classList.add('w-full', 'hidden', 'flex-col', 'rounded-4')
 
                 let dayCardHeader = document.createElement('header')
 
@@ -39,41 +45,50 @@ async function getWeatherData() {
                 if (condition.includes('sun') || condition.includes('sunny') || condition.includes('Sun') || condition.includes('Sunny')) { headerBackgroundImage = "sunny"}
                 else if (condition.includes('rain') || condition.includes('rainy') || condition.includes('Rain') || condition.includes('Rainy')) { headerBackgroundImage = "rainy"}
                 else { headerBackgroundImage = "cloudy"}
-                dayCardHeader.classList.add(`bg-${headerBackgroundImage}`)
+                dayCardHeader.classList.add(`bg-${headerBackgroundImage}`, 'bg-cover', 'bg-no-repeat', 'bg-center', 'rounded-banner', 'h-40', 'flex', 'items-end', 'p-4')
 
                 let cardConditionTag = document.createElement('span')
                 cardConditionTag.innerText = `${condition}`
+                cardConditionTag.classList.add('text-white', 'text-base', 'capitalize', 'font-medium')
 
                 dayCardHeader.appendChild(cardConditionTag)
 
 
                 let dayInfos = document.createElement('div')
+                dayInfos.classList.add('flex', 'items-center', 'justify-between', 'px-4', 'py-10')
 
                 let cardTitle = document.createElement('h3')
                 cardTitle.innerText = `${date}`
+                cardTitle.classList.add('font-bold')
 
                 let cardTempTag = document.createElement('p')
                 cardTempTag.innerText = `${minTemp}°C - ${maxTemp}°C`
+                cardTempTag.classList.add('font-bold')
 
                 dayInfos.appendChild(cardTitle)
                 dayInfos.appendChild(cardTempTag)
 
 
                 let launchInfos = document.createElement('div')
+                launchInfos.classList.add('flex', 'items-center', 'w-full')
                 let launchInfoAside = document.createElement('aside')
 
                 let launchBackgroundImage;
                 if (launchCondition.includes('sun') || launchCondition.includes('sunny') || launchCondition.includes('Sun') || launchCondition.includes('Sunny')) { launchBackgroundImage = "sunny"}
                 else if (launchCondition.includes('rain') || launchCondition.includes('rainy') || launchCondition.includes('Rain') || launchCondition.includes('Rainy')) { launchBackgroundImage = "rainy"}
                 else { launchBackgroundImage = "cloudy"}
-                launchInfoAside.classList.add(`bg-${launchBackgroundImage}`)
+                launchInfoAside.classList.add(`bg-${launchBackgroundImage}`, 'w-4/12', 'px-4', 'py-8', 'bg-cover', 'bg-no-repeat', 'bg-center')
 
                 let launchTimeTag = document.createElement('h4')
                 launchTimeTag.innerText = `Launch`
+                launchTimeTag.classList.add('text-white', 'text-base', 'capitalize', 'font-medium')
 
                 launchInfoAside.appendChild(launchTimeTag)
 
                 let launchInfoDiv = document.createElement('div')
+                launchInfoDiv.id ='clip-path-div'
+                launchInfoDiv.classList.add('w-8/12', 'px-4', 'py-8')
+
                 let launchTempTag = document.createElement('h5')
                 launchTempTag.innerText = `${launchTemp}°C - ${launchCondition}`
 
@@ -84,20 +99,24 @@ async function getWeatherData() {
 
 
                 let dinnerInfos = document.createElement('div')
+                dinnerInfos.classList.add('flex', 'items-center', 'w-full')
                 let dinnerInfoAside = document.createElement('aside')
 
                 let dinnerBackgroundImage;
                 if (dinnerCondition.includes('sun') || dinnerCondition.includes('sunny') || dinnerCondition.includes('Sun') || dinnerCondition.includes('Sunny')) { dinnerBackgroundImage = "sunny"}
                 else if (dinnerCondition.includes('rain') || dinnerCondition.includes('rainy') || dinnerCondition.includes('Rain') || dinnerCondition.includes('Rainy')) { dinnerBackgroundImage = "rainy"}
                 else { dinnerBackgroundImage = "cloudy"}
-                dinnerInfoAside.classList.add(`bg-${dinnerBackgroundImage}`)
+                dinnerInfoAside.classList.add(`bg-${dinnerBackgroundImage}`, 'w-4/12', 'px-4', 'py-8', 'bg-cover', 'bg-no-repeat', 'bg-center')
 
                 let dinnerTimeTag = document.createElement('h4')
                 dinnerTimeTag.innerText = `Dinner`
+                dinnerTimeTag.classList.add('text-white', 'text-base', 'capitalize', 'font-medium')
 
                 dinnerInfoAside.appendChild(dinnerTimeTag)
 
                 let dinnerInfoDiv = document.createElement('div')
+                dinnerInfoDiv.id ='clip-path-div'
+                dinnerInfoDiv.classList.add('w-8/12', 'px-4', 'py-8')
                 let dinnerTempTag = document.createElement('h5')
                 dinnerTempTag.innerText = `${dinnerTemp}°C - ${dinnerCondition}`
 
